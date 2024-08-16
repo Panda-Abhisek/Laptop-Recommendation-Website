83a90c7f2450f7ed8b77cf39e84027c5f7b0a489
Panda__Abhisek <abhisekpanda114@gmail.com>
2024-08-16 16:16:27 +0530

    file structure added

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+//simple route
+app.get("/", (req, res) => {
+  res.send("Laptop Recommendation API");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT: ${PORT}`);
+});
